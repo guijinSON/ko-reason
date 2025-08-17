94dc27e5e4e86c0563a977718db9330a530aa989
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2025-08-17 13:45:38 +0900

    Create prepare_dataset.py

---
+++ prepare_dataset.py
@@ -0,0 +1,23 @@
+from datasets import load_dataset
+from tqdm.auto import tqdm
+import pandas as pd
+
+df = load_dataset(
+    "KoReason/ko-r1-3.2.5", split="train"
+).to_pandas()
+
+msg = {"messages": []}
+for _,row in tqdm(df.iterrows(), total=len(df)):
+    msg["messages"].append([
+        {
+            "role": "user",
+            "content": row.instruction
+        },
+        {
+            "role": "assistant",
+            "content": row.response
+        }
+    ])
+
+ds = pd.DataFrame(msg)
+ds.to_json("train.jsonl", lines=True, orient="records")
