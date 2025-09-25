13b8b087b1b50f0562ff8209529e7823eb43b9f9
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2025-09-25 18:49:19 +0900

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -3,8 +3,7 @@ from tqdm.auto import tqdm
 import pandas as pd
 
 df = load_dataset(
-    "KoReason/kor1-YiSang", split="train"
-    # "KoReason/ko-r1-3.2.5"
+    "XXXX/kor1-YiSang", split="train"
 ).to_pandas()
 
 msg = {"messages": []}
